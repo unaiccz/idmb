22580bcab5447b6a9d7c2815293b5df35c79b840
unaiccz <unaicc2003@gmail.com>
2023-05-04 15:05:18 +0200

    header a medias

--- src/components/Head.jsx
+++ src/components/Head.jsx
@@ -1,11 +1,21 @@
 import React from 'react'
 import Menu from './Menu'
 import {ImHome} from 'react-icons/im'
+import {HiInformationCircle} from 'react-icons/hi'
 function Head() {
   return (
     <div>
+      <div className='flex'>
+<Menu title="HOME" sdd="/" Icon={ImHome}/>
+<Menu title="ABOUT" sdd="/about" Icon={HiInformationCircle}/>
+
+      </div>
       <div>
-<Menu title="HOME" sdd="/" icon={ImHome}/>
+      <h2 className='text-2xl'>
+<span className='mx-4 bg-amber-500 rounded-md'>IMDb</span>
+<span>clone</span>
+
+      </h2>
       </div>
     </div>
   )
--- src/components/Menu.jsx
+++ src/components/Menu.jsx
@@ -1,10 +1,18 @@
 import React from 'react'
-
-function Menu({title, sdd,icon}) {
+import Link from 'next/link'
+function Menu({title, sdd,Icon}) {
   return (
-    <div>
-      
+<>
+<div>
+      <Link href={sdd} className='mx-8 lg:mx-12 hover:text-amber-500'>
+      <Icon className='text-2xl sm:hidden'/>
+      <p className='hidden sm:inline my-2 text-sm'>{title}</p>  
+      </Link>
     </div>
+    <div>
+
+    </div>
+</>
   )
 }
 
