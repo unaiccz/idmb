f4c53b227195f58504ee2ea81535d2829cb20369
unaiccz <unaicc2003@gmail.com>
2023-05-04 17:39:07 +0200

    header

--- src/components/Head.jsx
+++ src/components/Head.jsx
@@ -4,16 +4,19 @@ import {ImHome} from 'react-icons/im'
 import {HiInformationCircle} from 'react-icons/hi'
 function Head() {
   return (
-    <div>
+
+    <div className='flex justify-between items-center py-6'>
+      {/* menu component */}
       <div className='flex'>
 <Menu title="HOME" sdd="/" Icon={ImHome}/>
 <Menu title="ABOUT" sdd="/about" Icon={HiInformationCircle}/>
 
       </div>
-      <div>
+      {/* logo part */}
+      <div className='mr-4 mt-4 max-w-6xl sm:mx-auto'>
       <h2 className='text-2xl'>
-<span className='mx-4 bg-amber-500 rounded-md'>IMDb</span>
-<span>clone</span>
+<span className='bg-amber-500 rounded-lg py-1 px-2 mr-1'>IMDb</span>
+<span className='text-xl hidden sm:inline'>clone</span>
 
       </h2>
       </div>
