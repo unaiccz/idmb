06ca9a9f0bf34f603d76dcda03813a6e3b6c2cfc
unaiccz <unaicc2003@gmail.com>
2023-05-05 21:37:58 +0200

    navbar

---
+++ src/components/NavItem.jsx
@@ -0,0 +1,25 @@
+'use client'
+import React from 'react'
+import Link from 'next/link'
+import { useSearchParams } from "next/navigation";
+function NavItem({title, param}) {
+  const searchParams = useSearchParams();
+  const genre = searchParams.get("genre");
+  
+  console.log(genre);
+  return (
+    <div>
+    <Link href={`/?genre=${param}`} className={`
+    m-4 hover:text-amber-600 font-semibold p-2
+    
+    // //to make a variable do $
+    ${genre && genre === param && "underline underline-offset-8 decoration-4 decoration-amber-600 rounded-lg" }
+    `}
+    >
+    <h1>{title}</h1>
+    </Link>
+    </div>
+  )
+}
+
+export default NavItem
