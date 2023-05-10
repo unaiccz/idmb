11b2f47011fcab5c2474e874516f53d15c383660
unai calderon <88508192+unaiccz@users.noreply.github.com>
2023-05-10 14:07:33 +0200

    loading effect

---
+++ src/app/loading.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+function loading() {
+  return (
+    <div className='flex justify-center'>
+      <img src="/lod.svg" alt="loading..."  className='h-96'/>
+    </div>
+  )
+}
+
+export default loading
