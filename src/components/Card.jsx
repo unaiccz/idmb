c55f0ff8fbbf2baba4d976923e811476c9bbede5
unai calderon <88508192+unaiccz@users.noreply.github.com>
2023-05-11 16:43:18 +0200

    card text scrollbar

--- src/components/Card.jsx
+++ src/components/Card.jsx
@@ -1,6 +1,8 @@
 import React from 'react'
 import Link from 'next/link'
 import Image from 'next/image'
+import {FiThumbsUp} from 'react-icons/fi'
+import {FiThumbsDown} from 'react-icons/fi'
 function Card({item}) {
   return (
     <div className='cursor-pointer sm:p-3 sm:hover:shadow-slate-400 sm:shadow-md rounded-lg sm:border
@@ -12,8 +14,19 @@ height={300}
 className='sm:rounded-t-lg group-hover:opacity-60 transition-opacity delay-200'
 placeholder='blur'
 blurDataURL='/lod.vg'
-
+alt='movie'
+style={
+  {maxWidth:'100%', height:'auto'}
+}
 />
+<div>
+  <p className='line-clamp-2'>{item.overview}</p>
+  <h2 className='text-bold'>  {item.title}
+<p className='flex items-center '>   {item.release_date}
+<FiThumbsUp className='h-5 mr-1 ml-5'/>
+{item.vote_count}
+</p></h2>
+</div>
     </Link>
     </div>
   )
