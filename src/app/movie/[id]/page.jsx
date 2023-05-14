4300336396a8e22a94cc9aa69dae255ffdcb006f
unai calderon <88508192+unaiccz@users.noreply.github.com>
2023-05-14 19:11:01 +0200

    movie page finish

--- src/app/movie/[id]/page.jsx
+++ src/app/movie/[id]/page.jsx
@@ -11,26 +11,33 @@ console.log(movieId)
 const movie = await res.json()
 
 
-
+//  --------------------------------------------------------------------
   return (
-    <div className='w-full'>
-<div className='p-4 md:pt-8 flex flex-col md:flex-row items-center content-center max-w-6xl mx-auto md:space-x-6'>
-<Image src={`https://image.tmdb.org/t/p/original/${item.backdrop_path || item.poster_path}`}
-width={500}
-height={300}
-className='sm:rounded-t-lg group-hover:opacity-60 transition-opacity delay-200'
-placeholder='blur'
-blurDataURL='/lod.svg'
-alt='movie'
-style={
-  {maxWidth:'100%', height:'auto'}
-}
-/>
-</div>
 
-    </div>
-
-  )
+    <div className="w-full">
+      {/* image of the movie */}
+      <div className=' p-4 md:p-8 flex flex-col md:flex-row
+      
+      items-center content-center max-w-6xl mx-auto md:space-x-6'>
+        <Image  
+        src={`https://image.tmdb.org/t/p/original/${movie.backdrop_path}`}
+         alt="movie poster" 
+         width={500} 
+         height={300} 
+         className='sm:rounded-lg'
+         />
+         <div className="p-2">
+          <p><span className='text-font-semibold mb-4'>{movie.overview}</span></p>
+         <h2 className='text-lg text-bold mt-4'>{movie.title}</h2>
+         <p className='mt-4'>{movie.release_date}</p>
+         {/* now the ratins on a span */}
+          <span className='text-yellow-400'> Rating: {movie.vote_average}</span>
+         </div>
+      </div>
+      
+      
+  </div>
+   )
 }
 
 export default page
