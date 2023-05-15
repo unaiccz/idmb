98b0a8fdd9d4b1f3be2367b136320170659e3b01
unai calderon <88508192+unaiccz@users.noreply.github.com>
2023-05-15 14:00:13 +0200

    oo

--- src/components/Result.jsx
+++ src/components/Result.jsx
@@ -6,8 +6,8 @@ function Result({ result }) {
     <div>
       <div className=" sm:grid sm:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4 2xl:grid-cols-5 max-w-6xl mx-auto py-4">
         {result.map((item, id) => (
-          <div>
-            <Card key={item.id} item={item}/>
+          <div key={item.id}>
+            <Card  item={item}/>
           </div>
         ))}
       </div>
